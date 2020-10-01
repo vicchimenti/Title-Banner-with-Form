1acf3e6e1513f614597a8d0060c6e48dfc2d1498
Vic Chimenti <chimenti@seattleu.edu>
2020-10-01 08:25:03 -0700

    add scratch file for scripts

---
+++ scripts.js
@@ -0,0 +1,22 @@
+//             < !-- < iframe id = "t4-form-2861" width = "auto" height = "350" src = "https://us.formbank.us/embed/Mjg2MS1lbg==" frameborder = "1"
+// scrolling = "no" allowtransparency = "false" > 
+//                 <section role="form" class="formPlaceholderWrapper" id="id<t4 type='meta' meta='content_id' />" <t4 type="meta" meta="html_anchor" />
+//                 <div class="formPlaceholder standardContent">
+//                                                 <t4 type="content" name="Form T4 Tag" output="normal" modifiers="nav_sections" />
+//                                             </div>
+//                 </section >
+//             </iframe > -->
+
+
+
+
+// < !--
+//             < iframe id = "t4-form-2861" width = "auto" height = "350" src = "https://us.formbank.us/embed/Mjg2MS1lbg==" frameborder = "1"
+// scrolling = "no" allowtransparency = "true" ></iframe >
+//     <script>
+//         function uif(e) { var t = document.getElementById("t4-form-2861"); t.setAttribute("height", e.height) }
+//                 var domain = "https://us.formbank.us", iframe = document.getElementById("t4-form-2861").contentWindow;
+//                 setInterval(function () {iframe.postMessage("", domain)}, 3e3), window.addEventListener("message", function (e) {"https://us.formbank.us" === e.origin && uif(e.data)}, !1);
+//             </script>
+
+// -->
